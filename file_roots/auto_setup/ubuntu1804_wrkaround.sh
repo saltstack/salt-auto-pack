9534a0d138a2f86024f9ca0162bab28ede271bb6
Bryce Larson <blarson@saltstack.com>
2020-05-28 15:26:26 -0600

    updates for ubuntu2004

--- file_roots/auto_setup/ubuntu1804_wrkaround.sh
+++ file_roots/auto_setup/ubuntu1804_wrkaround.sh
@@ -2,7 +2,7 @@
 # param $1 minion_id
 fqdn_minion=$(salt $1 grains.get fqdn -l quiet --out=json |  jq  '.[]' | sed  's/,//' | sed 's/"//g')
 SERVER_LIST="ubuntu@$fqdn_minion"
-for h in $SERVER_LIST; 
+for h in $SERVER_LIST;
 do
    (ssh -i /srv/salt/auto_setup/jenkins-testing.pem -o StrictHostKeyChecking=no -o RequestTTY=yes -t $h &)
 done
